8bf62da190a72b4800a35588abf67f8f5df70a70
Shambhu <shambhu261290@gmail.com>
2021-04-14 15:43:18 +0530

    Employee Unit Test Implemented
    Signed-off-by: Shambhu <shambhu261290@gmail.com>

---
+++ CoreAPIWithUnitTest/BusinessAccessLayer/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessAccessLayer
+{
+    public class Employee
+    {
+        public string GetEmployees(int id)
+        {
+            var data = (from t in EmployeeList.EmployeeLists()
+                       where t.Id == id
+                       select t.Name).FirstOrDefault();
+            return data;
+        }
+    }
+}
---
+++ CoreAPIWithUnitTest/UnitTesting/UnitTest_Employee.cs
@@ -0,0 +1,44 @@
+﻿using BusinessAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class UnitTest_Employee
+    {
+        [TestMethod]
+        public void Employees()
+        {
+            //Arrange
+            Employee employee = new Employee();
+            int id = 101;
+            string expected = "Sam";
+
+            //Act
+            string actual = employee.GetEmployees(id);
+
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Employees_2()
+        {
+            //Arrange
+            Employee employee = new Employee();
+            int id = 104;
+            string expected = "";
+
+            //Act
+            string actual = employee.GetEmployees(id);
+
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
