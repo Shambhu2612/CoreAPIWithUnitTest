9dcfd12dea0230b6ae22e712a649453e4de6f7b3
Shambhu <shambhu261290@gmail.com>
2021-04-14 15:04:56 +0530

    Unit Test Developed for Calculate Project
    Signed-off-by: Shambhu <shambhu261290@gmail.com>

---
+++ CoreAPIWithUnitTest/BusinessAccessLayer/Calculate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessAccessLayer
+{
+    public class Calculate
+    {
+        public int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Substarction (int num1, int num2)
+        {
+            if (num2 > num1)
+                return -1;
+            return num1 - num2;
+        }
+
+        public int Mulitplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Dividion(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
